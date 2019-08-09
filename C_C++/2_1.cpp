9d149947af3261e76336d1b8c8564bb021c11a6a
ScarletRain <992127134@qq.com>
2019-08-10 01:30:56 +0800

    1

---
+++ C_C++/2_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+    int a;
+    printf("Input the number");
+    cin >> a;
+    if (a == 1 || a == 3 || a == 5)
+        
+        cout << "NO" << endl;
+    else
+        cout << "YES" << endl;
+    return 0;
+}
