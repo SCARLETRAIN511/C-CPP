1cc9b079e6675d54924c6e2b590dc91ff42e60ff
Jiaxuan Tang <41808009+SCARLETRAIN511@users.noreply.github.com>
2019-11-28 15:07:58 +0000

    1

---
+++ C_C++/CLion_projects_B/Test_B/cmake-build-debug/CMakeFiles/test_cv.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_cv.dir/main.cpp.obj"
+  "libtest_cv.dll.a"
+  "test_cv.exe"
+  "test_cv.exe.manifest"
+  "test_cv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_cv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
