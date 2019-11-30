9e4fd023884495a9da09f53275fd8bd8af222608
Jiaxuan Tang <41808009+SCARLETRAIN511@users.noreply.github.com>
2019-11-30 13:13:03 +0000

    1

--- C_C++/Complete1/max.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by tangj on 2019/11/21.
-//
-int max(int a,int b);
-
-struct  node{
-    int value;
-    char *name;
-};
--- C_C++/Complete1/min.h
+++
@@ -1,6 +0,0 @@
-//
-// Created by tangj on 2019/11/21.
-//
-extern  int gAll;
-int min(int a, int b);
-
