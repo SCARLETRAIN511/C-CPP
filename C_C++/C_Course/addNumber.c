1b95f71cb4db25517b88066e886bc183c50af198
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-07-11 16:26:30 +0800

    update

---
+++ C_C++/C_Course/addNumber.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int x,y;
+    int add;
+    x = 3;
+    y = 1;
+    add = x + y;
+    printf("The sum of the 2 numbers is %d\n",add);
+}
---
+++ C_C++/C_Course/class1.c
@@ -0,0 +1,8 @@
+//class from mooc
+//有且只能有一个main函数
+
+
+int main(){
+    printf("Hello world");
+    return 0;
+}
