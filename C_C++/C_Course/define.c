a6773dad2985d6e712855b77c91497a1b651e013
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-07-16 18:32:02 +0800

    update

--- C_C++/C_Course/define.c
+++ C_C++/C_Course/define.c
@@ -8,4 +8,14 @@ void main(){
     add = x + M;
     int a = 3;
     printf("add = %d\n",add);
+
+    int k = 2;
+    float b = 5.2,c;
+    c = k+b;
+    printf("The sum is %f\n",c);
+
+    int s=3,absa;
+    absa = (s>0?s:-s); //this is actually if condition a>0 absa = a, vice versa
+    printf("absa is %d",absa);
+
 }
