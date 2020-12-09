088fc9ecbcd533092e33ac107a3f026ab0bf78b4
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-12-09 18:59:57 +0800

    update

---
+++ C_C++/Cpp-small-projects/myCalculator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    double num1,num2;
+    cout << "Enter the first number" << endl;
+    cin >> num1;
+
+    cout << "Enter the second the number" << endl;
+    cin >> num2;
+
+    cout<< "The result is: " << endl;
+    cout << num1 + num2 << endl;
+
+    return 0;
+}
