ded684d1313dd34b6c45e76c0d7ff0ab558ce4ae
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-12-09 21:03:55 +0800

    update

--- C_C++/Cpp-small-projects/ArrayPractise.cpp
+++ C_C++/Cpp-small-projects/ArrayPractise.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 using namespace std;
 
+void sayHi(){
+    cout << "Hello wold" << endl;
+}
+
+double cube(double num){
+    double result = num * num *num;
+    return result;
+}
+
 int main(){
     int luckyNums[20] = {1,2,331,42,13,83};
     luckyNums[1] = 14;
     luckyNums[19] = 2;
     cout << luckyNums[25] << endl;
 
+    //call the functions
+    sayHi();
+    double answer = cube(5.0);
+    cout << answer << endl;
     return 0;
 }
