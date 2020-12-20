fd4ca6f57d8d24bb1b1a475da8ebb4146817b1cc
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-12-21 00:22:46 +0800

    update

--- C_C++/Cpp-small-projects/StringPratices.cpp
+++ C_C++/Cpp-small-projects/StringPratices.cpp
@@ -6,5 +6,12 @@ int main(){
     char s[10] = "Hello";
     char s2[] = "HelloY";
     cout << s2 << endl;
-    return 0;
+
+    //reading and printing string
+    char s3[50];
+    cout << "Enter your name "<<endl;
+    cin.getline(s3,50);
+    //cin.getline(s3,50);
+    cout << s3 << endl;
+
 }
