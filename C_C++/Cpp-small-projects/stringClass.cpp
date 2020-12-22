944418b58a351b3282c35604b24df2013a97644a
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-12-22 16:20:40 +0800

    update

--- C_C++/Cpp-small-projects/stringClass.cpp
+++ C_C++/Cpp-small-projects/stringClass.cpp
@@ -13,7 +13,24 @@ void stringOp1(){
 void stringOp2(){
     string s = "Hello";
     s.append("World");
-    cout << s<< endl;
+    //cout << s<< endl;
+    string s2 = "word";
+    s2.insert(2,"second");
+    cout << s2 << endl;
+
+    string s3 = "ABCDEFG";
+    s3.replace(3,4,"kk");
+    cout << s3 << endl;
+
+    string s4  = "How you";
+    s4.insert(3," area",4);
+    cout << s4 << endl;
+
+    string str1 = "Hello";
+    string str2 = "World";
+    str1.swap(str2);
+    cout << str1 << endl;
+    
 }
 
 int main(){
