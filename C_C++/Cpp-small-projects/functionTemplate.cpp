8702353c8761af3cb749437aa7c22d274510d4d9
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-12-25 00:25:27 +0800

    update

--- C_C++/Cpp-small-projects/functionTemplate.cpp
+++ C_C++/Cpp-small-projects/functionTemplate.cpp
@@ -8,6 +8,26 @@ T maxim(T a,T b){
     return a>b?a:b;
 }
 
+int maxNum(int a,int b,int c = 10){
+    return a>b && a>c? a:(b>c?b:c);
+}
+
+//passing by address
+//pointer parameters
+//modify the parameters
+void swap(int *a,int *b){
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 int main(){
     cout << maxim(17.5f,14.4f) <<endl;
+    cout << maxNum(17,3,188)<<endl;
+    int a = 4;
+    int b = 5;
+    swap(&a,&b);
+    cout << a << endl;
+    cout << b <<endl;
 }
