174c5d6cbc098eda354f8d4559c06069fa398c70
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-12-26 00:57:58 +0800

    update

--- C_C++/Cpp-small-projects/functionTemplate.cpp
+++ C_C++/Cpp-small-projects/functionTemplate.cpp
@@ -22,12 +22,21 @@ void swap(int *a,int *b){
     *b = temp;
 }
 
+//passing by reference
+void swapRef(int &a,int &b){
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
 int main(){
     cout << maxim(17.5f,14.4f) <<endl;
     cout << maxNum(17,3,188)<<endl;
     int a = 4;
     int b = 5;
-    swap(&a,&b);
+    //swap(&a,&b);
+    swapRef(a,b);
     cout << a << endl;
     cout << b <<endl;
 }
