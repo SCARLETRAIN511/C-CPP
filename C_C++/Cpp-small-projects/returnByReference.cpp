c238505373e347e8c89294ca3e857330e84e340b
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-12-31 00:05:04 +0800

    update

--- C_C++/Cpp-small-projects/returnByReference.cpp
+++ C_C++/Cpp-small-projects/returnByReference.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 using namespace std;
 
+int g = 5;
+
 int &fun1(int &a){
     cout << a;
     return a;
+}
+
+void change(){
+    g ++;
 }
 
 int main(){
     int x = 10;
     fun1(x) = 25;
     cout << x << endl;
+    cout << "g = " << g <<endl;
+    change();
+    cout << "g = " << g <<endl;
 }
