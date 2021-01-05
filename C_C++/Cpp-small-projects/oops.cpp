b94bcb5c9dafd5e1c3c6f25e7e8381bcfe76aca0
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2021-01-05 08:47:30 +0800

    update

--- C_C++/Cpp-small-projects/oops.cpp
+++ C_C++/Cpp-small-projects/oops.cpp
@@ -23,6 +23,16 @@ int main(){
 
     r2.length = 5;
     r2.breadth = 2;
+
+    //class pointer;
+    Rectangle r;
+    Rectangle *p;
+    p = &r;
+    r.length = 50;
+    r.breadth = 20;
+    p -> breadth = 30;
+    cout << r.breadth << endl;
+
     
     cout << r1.perimeter()<<endl;
     cout << r2.area() << endl;
