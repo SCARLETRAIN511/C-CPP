0ab1e1e70393cbc0f121e4288edc8d83834e4f88
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2021-01-06 19:02:00 +0800

    update

--- C_C++/Cpp-small-projects/oops.cpp
+++ C_C++/Cpp-small-projects/oops.cpp
@@ -23,6 +23,8 @@ int main(){
 
     r2.length = 5;
     r2.breadth = 2;
+    cout << "The perimeter of r1 is "<<r1.perimeter()<<endl;
+    cout << "The area of r1 is "<<r2.area() << endl;
 
     //class pointer;
     Rectangle r;
@@ -31,9 +33,22 @@ int main(){
     r.length = 50;
     r.breadth = 20;
     p -> breadth = 30;
-    cout << r.breadth << endl;
+    cout <<"The breadth of r is "<< r.breadth << endl;
 
+    //pointer to an object
+    //the object is initialized in the heap;
+    //Set the pointer
+    Rectangle *p1;
+    p1 = new Rectangle;
     
-    cout << r1.perimeter()<<endl;
-    cout << r2.area() << endl;
+    p1 -> length = 15;
+    p1 -> breadth = 10;
+    cout << "The area of p1 is "<<p1->area() << endl;
+
+    //set the pointer
+    Rectangle *p2 = new Rectangle();
+    
+    p2 ->length = 3;
+    p2 -> breadth = 2;
+    cout << "The area of p2 is " << p2->area() <<endl;
 }   
