b6beb86741eb8a8447fe16c6f93b1811d62b1a8b
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2021-01-06 19:29:30 +0800

    update

---
+++ C_C++/Cpp-small-projects/AccessirAndMutators.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle{
+    private:
+    int length;
+    int breadth;
+
+    public:
+    void setLength(int l){
+        if (l >= 0){
+            length = l;
+        }else{
+            length = 0;
+        }
+    }
+
+    void setBreadth(int b){
+        if (b >= 0){
+        breadth = b;
+        }else{
+            breadth = 0;
+        }
+    }
+
+    int getLength(){
+        return length;
+    }
+
+    int getBreadth(){
+        return breadth;
+    }
+
+    int area(){
+        return length * breadth;
+    }
+    int perimeter(){
+        return 2*(length + breadth);
+    }
+};
+
+int main(){
+    Rectangle r1;
+    r1.setBreadth(-5);
+    r1.setLength(-3);
+    cout << "The area of r1 is "<<r1.area()<<endl;
+    cout << "The perimeter of r1 is " << r1.perimeter()<<endl;
+
+}
