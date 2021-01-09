f43ce90ca781b5016f6cd9983eecd439db2f1087
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2021-01-09 21:23:35 +0800

    update

--- C_C++/Cpp-small-projects/OperatorOverloading.cpp
+++ C_C++/Cpp-small-projects/OperatorOverloading.cpp
@@ -31,14 +31,57 @@ class Complex{
         }
 };
 
+class Complex2{
+    private:
+        int real;
+        int img;
+    
+    public:
+        Complex2(int r=0,int i=0){
+            real = r;
+            img = i;
+        }
+        Complex2 add(Complex2 x){
+            Complex2 temp;
+            temp.real = real+x.real;
+            temp.img = img+x.img;
+            return temp;
+        }
+        int getReal(){
+            return real;
+        }
+        int getImg(){
+            return img;
+        }
+        //friend function
+        friend Complex2 operator+(Complex2 n1,Complex2 n2);
+};
+
+//as friend function, no scope resolution would be used
+Complex2 operator+(Complex2 n1,Complex2 n2){
+        Complex2 t;
+        t.real = n1.getReal() + n2.getReal();
+        t.img = n1.getImg() + n2.getImg();
+        return t;
+}
+
 int main(){
     Complex c1(1,-2);
     Complex c2(1,5);
     Complex c3 = c1.add(c2);
     cout << c3.getImg()<<endl;
     cout << c3.getReal()<<endl;
+    cout << "Using operator overload"<<endl;
     Complex c4 = c1+c2;
     cout << c4.getImg()<<endl;
     cout << c4.getReal()<<endl;
+
+    cout << "Using friend function"<<endl;
+    Complex2 n1(2,5);
+    Complex2 n2(1,3);
+    Complex2 n3 = n1 + n2;
+    Complex2 n4 = operator+(n1,n2);
+    cout << n4.getImg()<<endl;
+    cout << n4.getReal()<<endl;
 
 }
