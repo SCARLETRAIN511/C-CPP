628d6405eb45ac78c6e13a691c44b13d38071c9c
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2021-01-12 19:24:58 +0800

    update

--- C_C++/Cpp-small-projects/Inheritence.cpp
+++ C_C++/Cpp-small-projects/Inheritence.cpp
@@ -18,7 +18,41 @@ class Derived:public Base{
     }
 };
 
-int main(){
+class Rectangle{
+    private:
+        int length;
+        int breadth;
+
+    public:
+        Rectangle(int l = 0,int b = 0){
+            length = l;
+            breadth = b;
+        }
+        int getLength(){
+            return length;
+        }
+        int getBreadth(){
+            return breadth;
+        }
+        void setLength(int l){
+            length = l;
+        }
+        void setBreadth(int b){
+            breadth = b;
+        }
+
+        int area(){
+            return length * breadth;
+        }
+        int perimeter(){
+            return 2*(length +  breadth);
+        }
+        bool isSquare(){
+            return length == breadth;
+        }
+};
+
+void useInheritence(){
     Derived d;
     d.x = 1;
     Base b;
@@ -27,4 +61,35 @@ int main(){
     d.y = 2;
     d.display();
     d.show();
+}
+
+class Cuboid:public Rectangle{
+    private:
+        int height;
+    public:
+        Cuboid(int l=0,int b =0,int h = 0){
+            height = h;
+            setLength(l);
+            setBreadth(b);           
+        }
+
+        int getHeight(){
+            return height;
+        }
+        void setHeight(int h){
+            height = h;
+        }
+        int volume(){
+            return getLength()*getBreadth()*getHeight();
+        }
+};
+
+
+int main(){
+    useInheritence();
+
+    Rectangle r(10,5);
+    cout << "The area of r is "<<r.area()<<endl;
+    Cuboid c1(2,3,5);
+    cout << "The volume of c1 is "<<c1.volume()<<endl;
 }
