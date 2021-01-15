e4e38013756a1a511d36b181f266b059d0b5ac72
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2021-01-15 21:18:15 +0800

    update

--- C_C++/Cpp-small-projects/abstractClass.cpp
+++ C_C++/Cpp-small-projects/abstractClass.cpp
@@ -24,8 +24,43 @@ class Derived:public Base{
     }
 };
 
+class Car{
+    public:
+    virtual void start() = 0;
+    virtual void stop() = 0;
+};
+
+class Innova:public Car{
+    public:
+    void start(){
+        cout <<"The innova starts"<<endl;
+    }
+    void stop(){
+        cout <<"The innova stops" <<endl;
+    }
+};
+
+class Swist:public Car{
+    public:
+    void start(){
+        cout <<"The Swist starts"<<endl;
+    }
+    void stop(){
+        cout <<"The Swist stops" <<endl;
+    }
+};
+
+
+
 int main(){
     Base *p = new Derived;
     p->fun1();
     p->fun2();
+
+    // can not do Car c1;
+    Car *c1 = new Innova();
+    c1->start();
+    Car *c2 = new Swist;
+    c2->stop();
+
 }
