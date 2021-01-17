f15eb82a5c2db191262af9d3a20bf0784c42c094
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2021-01-18 00:08:58 +0800

    update

--- C_C++/Cpp-small-projects/staticMembers.cpp
+++ C_C++/Cpp-small-projects/staticMembers.cpp
@@ -21,6 +21,18 @@ class Test{
 //can only be accessible by Test class
 int Test::count = 0;
 
+//static Members 2
+class Innova{
+    public:
+    static int price;
+
+    static int getPrice(){
+        return price;
+    }
+};
+
+int Innova::price = 10;
+
 int main(){
 
     Test t1;
@@ -30,4 +42,6 @@ int main(){
     cout << t1.getCount()<<endl;
     cout << Test::getCount()<<endl;
     cout << t2.getCount()<<endl;
+
+    cout<< "The price of innova is "<<Innova::getPrice()<<endl;
 }
