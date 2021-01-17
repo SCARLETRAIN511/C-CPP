b69e449dcac24440d808cfb03c7bf23f0c3ab5ae
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2021-01-17 23:47:08 +0800

    update

---
+++ C_C++/Cpp-small-projects/staticMembers.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+
+class Test{
+    private:
+        int a;
+        int b;
+    public:
+        static int count;
+        Test(){
+            a = 10;
+            b = 10;
+            count ++;
+        }
+};
+int Test::count = 0;
+
+int main(){
+    Test t1;
+    Test t2;
+
+    cout << t1.count<<endl;
+    cout << t2.count <<endl;
+}
