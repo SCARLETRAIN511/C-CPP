b663dd5a11bed3959a69fb209dbf26a3b5117597
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2021-01-18 00:03:20 +0800

    update

--- C_C++/Cpp-small-projects/staticMembers.cpp
+++ C_C++/Cpp-small-projects/staticMembers.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 using namespace std;
 
-
-
 class Test{
     private:
         int a;
@@ -14,13 +12,22 @@ class Test{
             b = 10;
             count ++;
         }
+
+        static int &getCount(){ 
+            int &ref = count;
+            return ref; 
+        }
 };
+//can only be accessible by Test class
 int Test::count = 0;
 
 int main(){
+
     Test t1;
     Test t2;
-
+    t1.getCount() = 19;
     cout << t1.count<<endl;
-    cout << t2.count <<endl;
+    cout << t1.getCount()<<endl;
+    cout << Test::getCount()<<endl;
+    cout << t2.getCount()<<endl;
 }
