a54a272bc6bec6c4e46a591b12b70f46acf83f4c
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2021-01-19 22:22:09 +0800

    update

--- C_C++/leetcode-cpp/findContinuousSequenceNum.cpp
+++ C_C++/leetcode-cpp/findContinuousSequenceNum.cpp
@@ -9,18 +9,24 @@ class Solution{
         vector<int> res;
         int sum = 0;
         int limit = (target - 1)/2;
+        //从1开始枚举
         for (int i = 1;i<=limit;i++){
+            //往后找连续数字 直到超出target或等于target;
             for (int j = i;;j++){
                 sum += j;
                 if (sum > target){
                     sum = 0;
+                    //如果超出，重新改变初始值
                     break;
                 }else if(sum == target){
                     res.clear();
+                    //把数字加入组合 res
                     for (int k = i;k<=j;k++){
                         res.emplace_back(k);
                     }
+                    //把组合加入数组
                     vec.emplace_back(res);
+                    //initialization
                     sum = 0;
                     break;
                 }
@@ -32,9 +38,10 @@ class Solution{
 
 int main(){
     Solution s;
-    vector<vector<int>> vec = s.findContinuousSequence(6);
+    vector<vector<int>> vec = s.findContinuousSequence(9);
+    //print the 2d vector;
     for (int i = 0; i < vec.size(); i++){
-        for(int j = 0; j < vec[0].size(); j++){
+        for(int j = 0; j < vec[i].size(); j++){
             cout << vec[i][j] << " ";
         }
         cout << endl;
