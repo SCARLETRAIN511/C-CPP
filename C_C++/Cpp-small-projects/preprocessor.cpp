89d12498d8498acff5d2b913dd4033e3e8549dd6
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2021-01-22 21:52:35 +0800

    update

---
+++ C_C++/Cpp-small-projects/preprocessor.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+#ifndef PI
+
+#define PI 3.1425 //symbolic constants
+#define SRR(x)(x*x)
+
+#endif // 
+
+
+
+int main(){
+    cout <<PI<<endl;
+    cout << SRR(5)<<endl;
+}
