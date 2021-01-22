af34b5d217056b5537e00df1899e61c268149d3f
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2021-01-22 16:54:45 +0800

    update

--- C_C++/Cpp-small-projects/Template.cpp
+++ C_C++/Cpp-small-projects/Template.cpp
@@ -16,24 +16,52 @@ void add(T x,R y){
 template <class T>
 class Stack{
     private:
-    T s[10];
-    int top;
+        T *stk;
+        int size;
+        int top;
+
     public:
+    Stack(int sz){
+        size = sz;
+        top = -1;
+        stk = new int[size];
+    }
+    
     void push(T x);
     T pop();
 };
 
 template<class T>
 void Stack<T>::push(T x){
-    
+    if (top == size - 1){
+        cout <<"stack is full";
+    }else{
+        top ++;
+        stk[top] = x;
+    }
 }
+
 template<class T>
 T Stack<T>::pop(){
-    
+    T x = 0;
+    if (top == -1){
+        cout <<"The stack is empty";
+    }else{
+        x = stk[top];
+        top --;
+    }
+    return x;
 }
 
 int main(){
     cout << maximum(10,15)<<endl;
     add(10,22.5);
+
+    Stack<int> s(10);
+    s.push(5);
+    s.push(3);
+    s.push(10);
+    cout << s.pop() <<endl;
+    cout << s.pop()<<endl;
 
 }
