27e924705349b3147b653c21ab00039b5a5c4047
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2021-01-23 19:14:45 +0800

    update

--- C_C++/Cpp-small-projects/streams.cpp
+++ C_C++/Cpp-small-projects/streams.cpp
@@ -2,3 +2,12 @@
 #include <fstream>
 using namespace std;
 
+int main(int argc, const char** argv) {
+    ofstream ofs("C:\\Users\\msipc\\Desktop\\code\\C-CPP\\C_C++\\Cpp-small-projects\\My.txt",ios::trunc);
+    ofs <<"John"<<endl;
+    ofs<<25<<endl;
+    ofs<<"cs"<<endl;
+    ofs.close();
+
+    return 0;
+}
