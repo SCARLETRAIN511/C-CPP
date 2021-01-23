e3daca9a6790bfa4da911e98c419a74263bb161c
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2021-01-23 18:30:40 +0800

    update

---
+++ C_C++/Cpp-small-projects/destructor.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Test{
+    private:
+        int *p;
+    public:
+        Test(){
+            p = new int[10];
+            cout << "Test Created"<<endl;
+        }
+        ~Test(){
+            delete []p;
+            cout <<"Test destroyed"<<endl;
+        }
+};
+
+void fun(){
+    //only the constructor will be called
+    Test *p = new Test();
+    delete p;
+
+    Test t;
+
+}
+
+int main(){
+    fun();
+}
