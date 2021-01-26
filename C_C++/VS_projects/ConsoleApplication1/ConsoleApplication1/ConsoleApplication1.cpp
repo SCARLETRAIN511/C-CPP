596f3b696c138b8202c6ce14f1865c5d62b283e0
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2021-01-26 20:28:10 +0800

    update

---
+++ C_C++/VS_projects/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,23 @@
+﻿// ConsoleApplication1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <Eigen/Dense>
+#include <cstdlib>
+
+using namespace std;
+using namespace Eigen;
+
+int main()
+{
+	MatrixXd m(2, 2);
+	m(0, 0) = 3;
+	m(1, 0) = 2.5;
+	m(0, 1) = -1;
+	m(1, 1) = m(1, 0) + m(0, 1);
+	std::cout << "Here is the matrix m:\n" << m << std::endl;
+	VectorXd v(2);
+	v(0) = 4;
+	v(1) = v(0) - 1;
+	std::cout << "Here is the vector v:\n" << v << std::endl;
+}
