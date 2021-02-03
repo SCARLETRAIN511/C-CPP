365446a30e0511c0fed1ed9f2f3af93289ee4dca
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2021-02-03 21:49:06 +0800

    update

--- C_C++/Cpp-data-structures/sturctureAsParameter.cpp
+++ C_C++/Cpp-data-structures/sturctureAsParameter.cpp
@@ -6,13 +6,21 @@ struct Rectangle{
     int breadth;
 };
 
-void area(struct Rectangle &r){
+//using reference
+void fun1(struct Rectangle &r){
     r.length ++;
+}
+
+//using pointer
+void changeLength(struct Rectangle *p,int l){
+    p->length = l;
 }
 
 int main(){
     struct Rectangle r1 =  {10,5};
-    area(r1);
+    fun1(r1);
     cout <<r1.length<<endl;
+    changeLength(&r1,14);
+    cout << r1.length<<endl;
 
 }
