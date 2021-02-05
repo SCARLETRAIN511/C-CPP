d2c1415e63040892aa62f2fdb2921b373e7c0377
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2021-02-05 16:53:46 +0800

    update

--- C_C++/Cpp-data-structures/Recursion.cpp
+++ C_C++/Cpp-data-structures/Recursion.cpp
@@ -16,8 +16,31 @@ void fun2(int n){
     }
 }
 
+//static variables
+int fun3(int n){
+    static int x = 0;
+    x ++;
+    if (n>0){
+        return fun3(n-1) + x;
+    }
+    return 0;
+}
+
+int x= 0;
+int fun4(int n){
+    x ++;
+    if (n>0){
+        return fun4(n-1) + x;
+    }
+    return 0;
+}
+
 int main(){
     int x = 3;
     fun1(x);
     fun2(x);
+
+    cout <<endl;
+    cout << fun4(4)<<endl;
+
 }
