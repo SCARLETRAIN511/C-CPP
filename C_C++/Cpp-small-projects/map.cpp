b735cde1dca3f31d5e848ef78e8309ae74eacba8
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2021-03-24 19:42:18 +0800

    update

--- C_C++/Cpp-small-projects/map.cpp
+++ C_C++/Cpp-small-projects/map.cpp
@@ -7,12 +7,11 @@ int main(){
     m.insert(pair<int,string>(1,"John"));
     m.insert(pair<int,string>(2,"Ravi"));
     m.insert(pair<int,string>(3,"Tony"));
-    map<int,string>::iterator itr;
-    for (itr = m.begin();itr != m.end();itr ++){
+    cout<<"using auto to define"<<endl;
+    for (auto itr = m.begin();itr != m.end();itr ++){
         cout << itr->first<<" "<<itr->second<<endl;
     }
-    map<int,string>::iterator itr1;
-    itr1 = m.find(2);
+    auto itr1 = m.find(2);
     cout << "Found value is "<<endl;
     cout <<itr1->first<<itr1->second<<endl;
 }
